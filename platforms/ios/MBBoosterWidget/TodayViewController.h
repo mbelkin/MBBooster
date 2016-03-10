7a673cf3075dc8d90913b20db9f29657e2db25e7
mbelkin <michael@belkininteractive.com>
2016-03-10 09:14:04 -0800

    added default today widget

---
+++ platforms/ios/MBBoosterWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  MBBoosterWidget
+//
+//  Created by Michael Belkin on 3/10/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
